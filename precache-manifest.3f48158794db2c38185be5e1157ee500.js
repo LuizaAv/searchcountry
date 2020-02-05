c90adf1980a181e964625387a8245d4a00fcccd6
Luiza Avetisyan <l.avetisyan7777@gmail.com>
2020-02-05 21:28:03 +0400

    Updates

---
+++ precache-manifest.3f48158794db2c38185be5e1157ee500.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a914339dc22074c740211de20de8b83",
+    "url": "/searchcountry/index.html"
+  },
+  {
+    "revision": "6ec3725ab92f43d6b57c",
+    "url": "/searchcountry/static/css/main.3f320d6a.chunk.css"
+  },
+  {
+    "revision": "cb470f2d5f3844f0ba48",
+    "url": "/searchcountry/static/js/2.10d1263e.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/searchcountry/static/js/2.10d1263e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ec3725ab92f43d6b57c",
+    "url": "/searchcountry/static/js/main.56dfafd6.chunk.js"
+  },
+  {
+    "revision": "89831f8391b55c0224e9",
+    "url": "/searchcountry/static/js/runtime-main.26c54f9d.js"
+  }
+]);
